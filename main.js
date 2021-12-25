e88c31a80c620695dccf9e34dcc78ed01f5d046a
Juancho <stefano.2000@hotmail.es>
2021-12-25 15:12:04 -0300

    desafio Crear un algoritmo simple

---
+++ main.js
@@ -0,0 +1,30 @@
+// Se pide el nombre al usuario
+const nombre = prompt("Ingrese su nombre:")
+
+// Se saluda al usuario en la consola
+console.log("Hola " + nombre + ", Bienvenido al curso")
+
+// Variable almacenada
+let num = 5;
+// Se pide un numero al usuario
+let numIngresado = prompt("Ingrese un numero");
+let num2 = parseInt(numIngresado); // Se transforma el string a un number
+
+// Sunma de los numeros
+let sumaNum = num + num2;
+
+// Se muestra el resultado en la consola
+console.log (sumaNum);
+
+// Se pide textos al usuario
+var texto1 = prompt("Ingrese un texto")
+var texto2 = prompt("Ingrese otro texto")
+
+// Se concacetnan los string
+var textoConcatenado = texto1 + texto2 ;
+
+// Alerta con los textos concetenados
+alert(textoConcatenado);
+
+
+
