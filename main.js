90e32fdd1367fe40fd62f17ea68719cf4e47fdfc
Juancho <stefano.2000@hotmail.es>
2021-12-27 16:20:07 -0300

    desafio 2

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 // Se pide el nombre al usuario
 const nombre = prompt("Ingrese su nombre:")
 
+if (nombre != ""){
 // Se saluda al usuario en la consola
-console.log("Hola " + nombre + ", Bienvenido al curso")
+    console.log("Hola " + nombre + ", Bienvenido al curso")
+}
+else
+//Se alerta al usuario
+alert("ERROR: No ingresaste el nombre de usuario")
 
-// Variable almacenada
-let num = 5;
+
 // Se pide un numero al usuario
 let numIngresado = prompt("Ingrese un numero");
 let num2 = parseInt(numIngresado); // Se transforma el string a un number
 
-// Sunma de los numeros
-let sumaNum = num + num2;
+if((numIngresado > 0) && (numIngresado < 100)) {
+    alert("Su numero es positivo y menor que 100")
+}
 
-// Se muestra el resultado en la consola
-console.log (sumaNum);
+else if(numIngresado == 100){
+    alert("Su numero es 100")
+}
 
-// Se pide textos al usuario
-var texto1 = prompt("Ingrese un texto")
-var texto2 = prompt("Ingrese otro texto")
+else if (numIngresado <0){
+    alert("Su numero es negativo")
+}
 
-// Se concacetnan los string
-var textoConcatenado = texto1 + texto2 ;
-
-// Alerta con los textos concetenados
-alert(textoConcatenado);
-
-
-
+else {
+    alert("Su numero es mayor que 100")
+}
