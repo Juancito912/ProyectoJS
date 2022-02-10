eab3ff917ab0fbb47d0aeeefdcec1b38ee870e56
Juancho <stefano.2000@hotmail.es>
2022-02-10 13:59:29 -0300

    desafio_DOM

--- desafios.js
+++ desafios.js
@@ -112,28 +112,20 @@ function menu_compras(){
 
     while (id_producto !== "ESC"){
     let mensaje = document.createElement("div");
-    mensaje.textContent = `Se ha añadido al carrito el alfajor ${computadoras[id_producto].gama}`;
+    mensaje.textContent = `Se ha añadido al carrito la computadora de gama ${computadoras[id_producto].gama}`;
     carritoEnHtml.appendChild(mensaje);
 
-    // Verificamos si ese tipo de alfajor ya se encuentra en el array
-    // con el método find()
-    // Este método en caso de dar true, nos devuelve el primer elemento del array
-    // que cumple con la condición de búsqueda
-    let alfajorEnCarrito = carrito.find((elemento) => {
+    
+    let computadora_en_carrito = carrito.find((elemento) => {
         if (elemento.id == id_producto) {
             return true;
         }
     });
 
-    if (alfajorEnCarrito) {
-      // Si el alfajor se encuentra en el carrito, alfajorEnCarrito devolverá
-      // true, por lo cual se ejecutará este bloque de código
-      // y se le sumará uno a la cantidad de esa marca en el carrito
-
-      // Primero debemos hallar el index donde se encuentra
-      // el alfajor en el carrito (ya que no va a ser el mismo que el del array alfajores);
+    if (computadora_en_carrito) {
+    
     let index = carrito.findIndex((elemento) => {
-        if (elemento.id === alfajorEnCarrito.id) {
+        if (elemento.id === computadora_en_carrito.id) {
             return true;
         }
     });
@@ -141,20 +133,13 @@ function menu_compras(){
     carrito[index].agregar_cantidad();
     carrito[index].calculo_precio_final();
     } else {
-      // El alfajor no se encuentra en el carrito, así que
-      // lo pusheamos al array asignándole la clase Alfajor
-      // para poder acceder a sus métodos
+    
         carrito.push(computadoras[id_producto]);
     }
 
-    // Una vez que terminamos de agregar el producto al carrito
-    // Volvemos a preguntar al usuario si desea seguir comprando
-    // para que se siga ejecutando el while
     id_producto = seleccionar_producto();
 }
 
-  // Una vez que finalizamos la compra informamos
-  // el precio total de la misma
     let precio_final = obtener_precio_total();
     imprimir_precio_total(precio_final);
 }
@@ -169,14 +154,12 @@ function obtener_precio_total() {
     return precio_final;
 }
 function imprimir_precio_total(precio_total) {
-    // Se imprime en el contenedor del carrito el precio total
-    // Creamos el div que contendrá el mensaje
+    
     let mensaje = document.createElement("div");
 
     // Agregamos el mensaje a dicho div
     mensaje.textContent = `El precio total de tu compra es de ${precio_total}`;
 
-    // Enlazamos el div del mensaje en el div con la info del carrito
     carritoEnHtml.appendChild(mensaje);
 }
 
