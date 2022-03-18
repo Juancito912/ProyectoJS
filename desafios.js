64161df706fb759c39f05594679924d37f398ad8
Juancho <stefano.2000@hotmail.es>
2022-03-18 19:03:47 -0300

    proyecto_final

--- desafios.js
+++ desafios.js
@@ -247,6 +247,28 @@ function chequear_storage (){
     }
     return compus;
 }
+let URL = "https://hp-api.herokuapp.com/api/characters";
+
+const personajes = [{name:"Seleccione su personaje"}];
+function lista(array) {
+    let opciones = "";
+    let i = 0;
+    array.forEach(personaje => opciones += `<option value="${i++}"> ${personaje.name} </option>`);
+    return `<select> ${opciones} </select>`;
+    
+}
+$(document).ready(()=>{
+    $.get(URL,function (data, state) {
+            if(state === "success"){
+                personajes.push(...data)
+                console.log(personajes)
+                $("#contenedor").prepend(`<div> Personajes: ${lista(personajes)}</div>`);
+                $("#carito").prepend(`<div id="salidaPersonajes"></div>`)
+            }
+        },
+        
+    );
+});
 
 imprimir_catalogo_HTML(computadoras);
 
